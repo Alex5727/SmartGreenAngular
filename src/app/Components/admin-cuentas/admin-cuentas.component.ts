5ea8e007d17ead9e07940db336f14615b0abef7e
xCorick <corickgonzalez123@gmail.com>
2025-03-13 14:20:21 -0700

    primer diseño

--- src/app/Components/admin-cuentas/admin-cuentas.component.ts
+++ src/app/Components/admin-cuentas/admin-cuentas.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-admin-cuentas',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-cuentas.component.html',
   styleUrl: './admin-cuentas.component.css'
 })
 export class AdminCuentasComponent {
+  ListUsers = [
+    {name:'usuario1',tipo:'admin'},
+    {name:'usuario2',tipo:'user'}
+  ]
+
+  adminUsers = this.ListUsers.filter(X => X.tipo == 'admin')
+  normalUsers = this.ListUsers.filter(x => x.tipo == 'user')
 
 }
