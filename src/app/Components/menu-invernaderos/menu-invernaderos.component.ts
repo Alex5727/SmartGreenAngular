8e4ca1750bef48706d57e77ffc9af30e118cdd15
Alex5727 <alejandrogrjalva@gmail.com>
2025-03-13 12:43:20 -0700

    fuaa we

--- src/app/Components/menu-invernaderos/menu-invernaderos.component.ts
+++ src/app/Components/menu-invernaderos/menu-invernaderos.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { CartaPlantaComponent } from '../carta-planta/carta-planta.component';
 
 @Component({
   selector: 'app-menu-invernaderos',
-  imports: [],
+  imports: [CartaPlantaComponent],
   templateUrl: './menu-invernaderos.component.html',
   styleUrl: './menu-invernaderos.component.css'
 })
