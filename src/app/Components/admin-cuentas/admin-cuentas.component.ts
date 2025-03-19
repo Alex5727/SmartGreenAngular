b6a7227b23dca535038f226006b9a1636801dcf5
xCorick <corickgonzalez123@gmail.com>
2025-03-18 23:04:58 -0700

    modal

--- src/app/Components/admin-cuentas/admin-cuentas.component.ts
+++ src/app/Components/admin-cuentas/admin-cuentas.component.ts
@@ -1,33 +1,40 @@
-import { TableModule } from 'primeng/table';
+import { Component } from '@angular/core';
 import { User } from '../models/user.model';
 import { AdminUserService } from '../services/admin-user.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { SiderbarComponent } from '../../siderbar/siderbar.component';
-
 
 @Component({
   selector: 'app-admin-cuentas',
-  imports: [CommonModule, TableModule],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './admin-cuentas.component.html',
-  styleUrl: './admin-cuentas.component.css'
+  styleUrls: ['./admin-cuentas.component.css']
 })
 export class AdminCuentasComponent {
   users: User[] = [];
   userAdmin: User[] = [];
   userscommon: User[] = [];
+  selectedUser: User | null = null;
 
-  constructor(private adminUserService: AdminUserService) { }
-  ngOnInit(){
+  constructor(private adminUserService: AdminUserService) {}
+
+  ngOnInit(): void {
     this.loadUsers();
   }
 
-  loadUsers() {
+  loadUsers(): void {
     this.adminUserService.getUsers().subscribe((data) => {
       this.users = data;
-      console.log(data);
       this.userAdmin = this.users.filter((user) => user.usuarioTipo === 'admin');
       this.userscommon = this.users.filter((user) => user.usuarioTipo === 'user');
     });
+  }
+
+  openModal(user: User): void {
+    this.selectedUser = user;
+  }
+
+  closeModal(): void {
+    this.selectedUser = null;
   }
 }
